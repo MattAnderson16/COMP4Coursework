b20588cd85dc830590c068e2ec91aac3ba90e58a
Matthew Anderson <30393@longroad.ac.uk>
2014-12-18 13:54:36 +0000

    edit data class

--- Implementation/edit_data_class.py
+++ Implementation/edit_data_class.py
@@ -6,6 +6,7 @@ import sys
 class EditData(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Edit Data")
         self.stacked_layout = QStackedLayout()
 
         self.create_data_selection_layout()
@@ -48,26 +49,120 @@ class EditData(QMainWindow):
 
             self.data_layout = QHBoxLayout()
             self.data_layout.addWidget(self.data)
-            self.data_layout.addWidget(self.data_type_dropdown)
+            self.data_layout.addWidget(self.data_dropdown)
 
-            self.button_layout = QHBoxLayout()
-            self.button_layout.addWidget(self.proceed_button)
-            self.button_layout.addWidget(self.back_button)
+            self.button_layout_a = QHBoxLayout()
+            self.button_layout_a.addWidget(self.proceed_button)
+            self.button_layout_a.addWidget(self.back_button_a)
 
             self.data_selection_layout = QVBoxLayout()
             self.data_selection_layout.addLayout(self.data_table_layout)
             self.data_selection_layout.addLayout(self.data_type_layout)
-            self.data_selection_layout.addLayout(data_layout)
-            self.data_selection_layout.addLayout(button_layout)
+            self.data_selection_layout.addLayout(self.data_layout)
+            self.data_selection_layout.addLayout(self.button_layout_a)
             
             self.data_selection_widget = QWidget()
             self.data_selection_widget.setLayout(self.data_selection_layout)
+
+            self.proceed_button.clicked.connect(self.create_edit_data_layout)
+            self.back_button_a.clicked.connect(self.close)
         else:
             self.stacked_layout.setCurrentIndex(0)
 
     def create_edit_data_layout(self):
-        pass
+        if not hasattr(self,"edit_data_layout"):
+            self.data_name_a = QLabel()
+            self.data_name_b = QLabel()
+            self.data_line_a = QLabel()
+            self.data_line_b = QLabel()
+            self.edit_line_a = QPushButton("Edit")
+            self.edit_line_b = QPushButton("Edit")
+            self.confirm_button_a = QPushButton("Confirm")
+            self.back_button_b = QPushButton("Back")
+
+            self.data_line_a_layout = QHboxLayout()
+            self.data_line_a_layout.addWidget(self.data_name_a)
+            self.data_line_a_layout.addWidget(self.data_line_a)
+            self.data_line_a_layout.addWidget(self.edit_line_a)
+
+            self.data_line_b_layout = QHBoxLayout()
+            self.data_line_b_layout.addWidget(self.data_name_b)
+            self.data_line_b_layout.addWidget(self.data_line_b)
+            self.data_line_b_layout.addWidget(self.edit_line_b)
+
+            self.button_layout_b = QHBoxLayout()
+            self.button_layout_b.addWidget(self.confirm_button_a)
+            self.button_layout_b.addWidget(self.back_button_b)
+
+            self.edit_data_layout = QVBoxLayout()
+            self.edit_data_layout.addLayout(self.data_line_a_layout)
+            self.edit_data_layout.addLayout(self.data_line_b_layout)
+            self.edit_data_layout.addLayout(self.button_layout_b)
+            
+            self.edit_data_widget = QWidget()
+            self.edit_data_widget.setLayout(self.edit_data_layout)
+
+            self.edit_line_a.clicked.connect(self.edit_data_a)
+            self.edit_line_b.clicked.connect(self.edit_data_b)
+            self.confirm_button_a.clicked.connect(self.create_confirmation_layout)
+            self.back_button_b.clicked.connect(self.create_data_selection_layout)
+        else:
+            self.stacked_layout.setCurrentIndex(1)
 
     def create_confirmation_layout(self):
-        pass
+        if not hasattr(self,"confirmation_layout"):
+            self.confirm_button_b = QPushButton("Confirm")
+            self.back_button_c = QPushButton("Back")
+
+            self.button_layout_c = QHBoxLayout()
+            self.button_layout_c.addWidget(self.confirm_button_b)
+            self.button_layout_c.addWidget(self.back_button_c)
+
+            self.confirmation_layout = QVBoxLayout()
+            self.confirmation_layout.addLayout(self.button_layout_c)
+
+            self.confirmation_widget = QWidget()
+            self.confirmation_widget.setLayout(self.confirmation_layout)
+
+            self.confirm_button_b.clicked.connect(self.edit_data)
+            self.back_button_c.clicked.connect(self.create_edit_data_layout)
+        else:
+            self.stacked_layout.setCurrentIndex(2)
+
+    def edit_line_a(self):
+        if not hasattr(self,"edit_line_a_layout"):
+            self.current_data = QLabel("Current Data:")
+            self.new_data =  QLabel("New Data:")
+            self.new_data_edit = QLineEdit()
+            self.confirm_button_c = QPushButton("Confirm")
+            self.back_button_d = QPushButton("Back")
+
+            self.new_data_layout = QGridLayout()
+            self.new_data_layout.addWidget(self.current_data,1,1)
+            self.new_data_layout.addWidget(self.new_data,2,1)
+            self.new_data_layout.addWidget(self.data_line_a,1,2)
+            self.new_data_layout.addWidget(self.new_data_edit,2,2)
+
+            self.button_layout_d = QHBoxLayout()
+            self.button_layout_d.addWidget(self.confirm_button_c)
+            self.button_layout_d.addWidget(self.back_button_d)
+
+            self.edit_line_a_layout = QVBoxLayout()
+            self.edit_line_a_layout.addLayout(self.new_data_layout)
+            self.edit_line_a_layout.addLayout(self.button_layout_d)
+
+            self.edit_line_a_widget = QWidget()
+            self.edit_line_a_widget.setLayout(self.edit_line_a_layout)
+
+            self.stacked_layout.addWidget(self.edit_line_a_widget)
+        self.stacked_layout.setIndex(4)
         
+    def edit_data(self):
+        pass
+
+if __name__ == "__main__":
+    application = QApplication(sys.argv)
+    window = EditData()    
+    window.show()
+    window.raise_()
+    application.exec_()
